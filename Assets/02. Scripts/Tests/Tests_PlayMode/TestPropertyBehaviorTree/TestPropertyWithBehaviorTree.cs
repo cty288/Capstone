98e862944ab4f431a2b05db9133a4e5569bb18fd
cty288 <tc3158@nyu.edu>
2023-07-13 23:43:56 +0800

    temp save

---
+++ Assets/02. Scripts/Tests/Tests_PlayMode/TestPropertyBehaviorTree/TestPropertyWithBehaviorTree.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+public class TestPropertyWithBehaviorTree : MonoBehaviour {
+    [SerializeField] private SharedVariable<int> test;
+}
