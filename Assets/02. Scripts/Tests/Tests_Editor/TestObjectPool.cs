838c4ec62e765569e4a3e2fdbca849edc72b806b
cty288 <tc3158@nyu.edu>
2023-08-18 22:58:38 +0800

    test for pools

---
+++ Assets/02. Scripts/Tests/Tests_Editor/TestObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using MikroFramework.Pool;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+
+namespace Tests.Tests_Editor {
+
+	public class MyClass : IPoolable {
+		public void OnRecycled() {
+			number = 0;
+		}
+
+		public bool IsRecycled { get; set; }
+
+		public int number;
+		
+		public void RecycleToCache() {
+			SafeObjectPool<MyClass>.Singleton.Recycle(this);
+		}
+
+		public static MyClass Allocate(int number) {
+			MyClass c = SafeObjectPool<MyClass>.Singleton.Allocate();
+			c.number = number;
+			return c;
+		}
+	}
+	
+	
+	
+	public class TestObjectPool {
+		[Test]
+		public void TestObjectPoolBasic() {
+			//optional: initialization
+			SafeObjectPool<MyClass>.Singleton.Init(10,100);
+			
+			//allocate
+			MyClass myClass = MyClass.Allocate(100);
+			
+			//tests
+			Assert.IsNotNull(myClass);
+			Assert.AreEqual(100, myClass.number);
+			
+			//recycle
+			SafeObjectPool<MyClass>.Singleton.Recycle(myClass); //or myClass.RecycleToCache();
+			
+			//tests
+			Assert.IsTrue(myClass.IsRecycled);
+			Assert.AreEqual(0, myClass.number);
+		}
+	}
+}
