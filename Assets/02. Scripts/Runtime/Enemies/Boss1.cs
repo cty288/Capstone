e3ba27b7a8cea4183c574065d265f241fb08991e
IDislikeName <yilaisun@yahoo.com>
2023-09-07 22:37:31 -0400

    implementing collision and stuff

--- Assets/02. Scripts/Runtime/Enemies/Boss1.cs
+++ Assets/02. Scripts/Runtime/Enemies/Boss1.cs
@@ -1,4 +1,5 @@
 using Framework;
+using System.Collections.Generic;
 using MikroFramework.BindableProperty;
 using Runtime.DataFramework.Entities.ClassifiedTemplates.Factions;
 using Runtime.DataFramework.Properties;
@@ -10,7 +11,11 @@ using Runtime.Enemies.Model.Properties;
 using Runtime.Enemies.ViewControllers.Base;
 using Runtime.Utilities.ConfigSheet;
 using Runtime.Enemies;
+using Runtime.Utilities.AnimationEvents;
+using Runtime.Utilities.Collision;
 using UnityEngine;
+using UnityEngine.AI;
+
 using PropertyName = Runtime.DataFramework.Properties.PropertyName;
 
 namespace Runtime.Enemies
@@ -18,13 +23,13 @@ namespace Runtime.Enemies
     public class Boss1Entity : EnemyEntity<Boss1Entity>
     {
         [field: ES3Serializable]
-        public override string EntityName { get; protected set; } = "Boss1";
+        public override string EntityName { get; set; } = "Boss1";
 
         [field: ES3Serializable]
         public bool ShellStatus { get; set; } = true;
         public override void OnRecycle()
         {
-            
+
         }
 
         protected override void OnEnemyRegisterAdditionalProperties()
@@ -44,10 +49,43 @@ namespace Runtime.Enemies
 
         
     }
-    public class Boss1 : AbstractEnemyViewController<Boss1Entity>
+    public class Boss1 : AbstractEnemyViewController<Boss1Entity>, IHurtResponder, IHitResponder
     {
-        [Bind("ShellStatus", null,nameof(OnShellStatusChanged))]
-        public bool ShellStatus { get; }
+        [Header("HitResponder_Info")]
+        [SerializeField] private int m_damage = 10;
+        public Animator animator;
+        public AnimationSMBManager animationSMBManager;
+        public NavMeshAgent agent;
+        public int Damage => m_damage;
+        [SerializeField] private HitBox hitbox_roll;
+
+        private List<GameObject> hitObjects = new List<GameObject>();
+        private HitDetectorInfo hitDetectorInfo;
+        [Header("Hurtresponder_Info")]
+        [SerializeField] private List<HurtBox> hurtBoxes = new List<HurtBox>();
+
+        public bool CheckHit(HitData data)
+        {
+            if (data.Hurtbox.Owner == gameObject) { return false; }
+            else if (hitObjects.Contains(data.Hurtbox.Owner)) { return false; }
+            else { return true; }
+        }
+
+        public bool CheckHurt(HitData data)
+        {
+            return true;
+        }
+
+        public void HitResponse(HitData data)
+        {
+            hitObjects.Add(data.Hurtbox.Owner);
+        }
+
+        public void HurtResponse(HitData data)
+        {
+            BoundEntity.TakeDamage(data.Damage,null);
+        }
+
         protected override void OnEntityDie(IBelongToFaction damagedealer)
         {
             throw new System.NotImplementedException();
@@ -60,7 +98,20 @@ namespace Runtime.Enemies
 
         protected override void OnEntityStart()
         {
-            throw new System.NotImplementedException();
+            hurtBoxes = new List<HurtBox>(GetComponentsInChildren<HurtBox>());
+            foreach (HurtBox hurtBox in hurtBoxes)
+            {
+                hurtBox.HurtResponder = this;
+            }
+
+            //Animation-related.
+            // animator = GetComponent<Animator>();
+            animationSMBManager = GetComponent<AnimationSMBManager>();
+            animationSMBManager.Event.AddListener(OnAnimationEvent);
+
+            //Collision-related.
+            hitbox_roll.HitResponder = this;
+            hitDetectorInfo = new HitDetectorInfo();
         }
 
         protected override void OnEntityTakeDamage(int damage, int currenthealth, IBelongToFaction damagedealer)
@@ -79,6 +130,20 @@ namespace Runtime.Enemies
         protected void OnShellStatusChanged(bool oldValue,bool newValue)
         {
             Debug.Log("Shell status changed to:" + newValue);
+        }
+        private void Update()
+        {
+
+        }
+
+        public void OnAnimationEvent(string eventName)
+        {
+            // Debug.Log("Animation Event: " + eventName);
+            switch (eventName)
+            {
+                default:
+                    break;
+            }
         }
     }
 }
