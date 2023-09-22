f210dc03d2eb4f6b97b845f25fe135d79c4c7d5b
cty288 <chentim.sh@gmail.com>
2023-09-21 22:41:01 -0400

    fix health bar

--- Assets/02. Scripts/Runtime/Enemies/ViewControllers/Base/AbstractNormalEnemyViewController.cs
+++ Assets/02. Scripts/Runtime/Enemies/ViewControllers/Base/AbstractNormalEnemyViewController.cs
@@ -9,6 +9,8 @@ namespace Runtime.Enemies.ViewControllers.Base {
 		where T : class, IEnemyEntity, new()  {
 		[SerializeField] private string healthBarPrefabName = "EnemyHealthBar";
 		[SerializeField] private Transform healthBarSpawnPoint = null;
+		
+		private HealthBar currentHealthBar = null;
 		protected override HealthBar OnSpawnHealthBar() {
 			HealthBar bar = HUDManager.Singleton
 				.SpawnHUDElement(healthBarSpawnPoint, healthBarPrefabName, HUDCategory.HealthBar, true)
@@ -16,12 +18,29 @@ namespace Runtime.Enemies.ViewControllers.Base {
 			
 			//set its scale to health bar spawn point's scale
 			bar.transform.localScale = healthBarSpawnPoint.localScale;
-			
+			bar.gameObject.SetActive(false);
+			currentHealthBar = bar;
 			return bar;
+		}
+
+		public override void OnPointByCrosshair() {
+			base.OnPointByCrosshair();
+			if (currentHealthBar) {
+				currentHealthBar.gameObject.SetActive(true);
+			}
+		}
+
+		public override void OnUnPointByCrosshair() {
+			base.OnUnPointByCrosshair();
+			if (currentHealthBar) {
+				currentHealthBar.gameObject.SetActive(false);
+			}
 		}
 
 		protected override void OnDestroyHealthBar(HealthBar healthBar) {
 			HUDManager.Singleton.DespawnHUDElement(healthBarSpawnPoint, HUDCategory.HealthBar);
+			currentHealthBar = null;
 		}
+		
 	}
 }
