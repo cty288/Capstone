3bedb498dad15870173571314278e7092cbe3d4c
cty288 <chentim.sh@gmail.com>
2023-09-29 01:40:39 -0400

    property setup

---
+++ Assets/02. Scripts/Runtime/Player/Model/Properties/AdditionalGravity.cs
@@ -0,0 +1,19 @@
+﻿using Runtime.DataFramework.Properties;
+
+namespace Runtime.Player.Properties {
+	public interface IAdditionalGravity : IProperty<float>, ILoadFromConfigProperty { }
+
+	public class AdditionalGravity: AbstractLoadFromConfigProperty<float>, IAdditionalGravity {
+		protected override IPropertyDependencyModifier<float> GetDefautModifier() {
+			return new EmptyModifier<float>();
+		}
+
+		protected override PropertyName GetPropertyName() {
+			return PropertyName.additional_gravity;
+		}
+
+		public override PropertyNameInfo[] GetDefaultDependentProperties() {
+			return null;
+		}
+	}
+}
