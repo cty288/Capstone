cd149a7511d01d9e40b05db403ec4beb238eaae6
Joon Lee <wl2357@nyu.edu>
2023-10-08 01:08:36 +0900

    worm attack logic

---
+++ Assets/02. Scripts/Runtime/BehaviorDesigner/Tasks/EnemyAction/WormAttackRapidFire.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using a;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using MikroFramework;
+using MikroFramework.Pool;
+using Runtime.DataFramework.Entities.ClassifiedTemplates.Damagable;
+using Runtime.DataFramework.Entities.ClassifiedTemplates.Factions;
+using Runtime.Enemies;
+using Runtime.Utilities.Collision;
+using UnityEngine;
+using Runtime.Temporary.Weapon;
+using Runtime.Weapons.ViewControllers.Base;
+using Runtime.Enemies.SmallEnemies;
+
+
+namespace Runtime.BehaviorDesigner.Tasks.EnemyAction
+{
+    public class WormAttackRapidFire : EnemyAction<HunterWormEntity>
+    {
+        public SharedGameObject bulletPrefab;
+        //public int bulletCount;
+        //public float spawnInterval;
+        private bool ended;
+        //public int bulletSpeed;
+
+
+
+        private Transform playerTrans;
+        //public SharedTransform playerTrans;
+
+        private SafeGameObjectPool pool;
+        private int bulletCount;
+        private float spawnInterval;
+        private float bulletSpeed;
+        private float bulletAccuracy;
+        private GameObject player;
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            pool = GameObjectPoolManager.Singleton.CreatePool(bulletPrefab.Value, 50, 100);
+            playerTrans = GetPlayer().transform;
+            player = GetPlayer();
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            ended = false;
+            spawnInterval = enemyEntity.GetCustomDataValue<float>("attack", "spawnInterval");
+            bulletCount = enemyEntity.GetCustomDataValue<int>("attack", "ammoSize");
+            
+            bulletSpeed = enemyEntity.GetCustomDataValue<float>("attack", "bulletSpeed");
+            bulletCount = enemyEntity.GetCustomDataValue<int>("attack", "bulletCount");
+            bulletAccuracy = enemyEntity.GetCustomDataValue<float>("attack", "bulletAccuracy");
+            StartCoroutine(RF());
+        }
+        public override TaskStatus OnUpdate()
+        {
+            if (ended)
+                return TaskStatus.Success;
+            else
+                return TaskStatus.Running;
+
+        }
+        IEnumerator RF()
+        {
+            for (int i = 0; i < bulletCount; i++)
+            {
+                SpawnBullet();
+                yield return null;
+            }
+            ended = true;
+        }
+        void SpawnBullet()
+        {
+            UnityEngine.GameObject b = pool.Allocate();
+
+            // Calculate a random rotation offset within a specified range
+            float randomAngle = Random.Range(-20, 20);
+
+            // Apply the rotation offset to the bullet's rotation
+            Quaternion randomRotation = Quaternion.Euler(randomAngle, randomAngle, 0);
+            b.transform.rotation *= randomRotation;
+
+            b.GetComponent<IBulletViewController>().Init(enemyEntity.CurrentFaction.Value,
+                enemyEntity.GetCustomDataValue<int>("attack", "bulletDamage"),
+                gameObject, gameObject.GetComponent<ICanDealDamage>());
+
+            b.GetComponent<WormBullet>().SetData(bulletSpeed, player, bulletAccuracy);
+        }
+
+        public override void OnEnd()
+        {
+            base.OnEnd();
+            StopAllCoroutines();
+        }
+    }
+}
---
+++ Assets/02. Scripts/Runtime/Enemies/SmallEnemies/WormBullet.cs
@@ -0,0 +1,58 @@
+using Runtime.Utilities.Collision;
+using Runtime.Weapons.ViewControllers.Base;
+using UnityEngine;
+
+namespace a
+{
+    public class WormBullet : AbstractBulletViewController
+    {
+        private float bulletSpeed;
+        private GameObject player; // Reference to the player's transform
+        private float rotationSpeed;
+        private float life = 2f;
+
+
+        private void Update()
+        {
+            if (player != null)
+            {
+                Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+                Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+
+                // Adjust the rotation speed by multiplying with rotationSpeed
+                float t = rotationSpeed * Time.deltaTime;
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
+
+                transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+            }
+            life -= Time.deltaTime;
+            if(life < 0)
+            {
+                //how do i put this back to the pool?
+                Destroy(this.gameObject);
+            }
+        }
+
+        public void SetData(float bulletSpeed, GameObject player , float rotationSpeed)
+        {
+            this.bulletSpeed = bulletSpeed;
+            this.player = player;
+            this.rotationSpeed = rotationSpeed;
+        }
+
+        protected override void OnHitResponse(HitData data)
+        {
+            
+        }
+
+        protected override void OnHitObject(Collider other)
+        {
+            
+        }
+
+        protected override void OnBulletRecycled()
+        {
+            
+        }
+    }
+}
