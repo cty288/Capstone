0a96a68153ac30f4531f41bf18cf20d57c23d298
tinybitofheaven <abc9295@nyu.edu>
2023-10-10 14:10:36 -0400

    spawning start

---
+++ Assets/02. Scripts/Runtime/Spawning/DirectorViewController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using MikroFramework.Architecture;
+using UnityEngine;
+using Runtime.DataFramework.Entities;
+using Runtime.DataFramework.Properties.CustomProperties;
+using Runtime.DataFramework.ViewControllers.Entities;
+using Runtime.Enemies.Model;
+using Runtime.Utilities.ConfigSheet;
+
+namespace Runtime.Spawning
+{
+    public interface IDirectorViewController : IEntityViewController {
+        IDirectorEntity DirectorEntity => Entity as IDirectorEntity;
+    }
+    
+    public abstract class AbstractDirectorViewController<T>  : AbstractBasicEntityViewController<DirectorEntity> where  T : class, IDirectorEntity, new() 
+    {
+        protected IDirectorModel directorModel;
+        
+        protected override void Awake() {
+            base.Awake();
+            directorModel = this.GetModel<IDirectorModel>();
+        }
+        
+        protected override IEntity OnBuildNewEntity()
+        {
+            DirectorBuilder<T> builder = directorModel.GetDirectorBuilder<T>();
+            return OnInitDirectorEntity(builder);
+        }
+        
+        protected abstract IEntity OnInitDirectorEntity(DirectorBuilder<T> builder);
+
+        protected override void OnEntityStart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnBindEntityProperty()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/02. Scripts/Runtime/Spawning/Lottery.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime.Spawning
+{
+    public class Lottery<T>
+    {
+        private Dictionary<Vector2, T> entityWeights;
+        
+        public void SetCards(List<T> spawnCards)
+        {
+            int totalWeight = 0;
+            foreach (T entity in spawnCards)
+            {
+                totalWeight += entity.GetRealWeight();
+            }
+            
+            float currentWeight = 0f;
+            foreach (T entity in spawnCards)
+            {
+                entityWeights.Add(new Vector2(currentWeight, currentWeight + entity.GetRealWeight()), entity);
+                currentWeight += entity.GetRealWeight();
+            }
+        }
+
+        public T PickNextCard()
+        {
+            float randomWeight = Random.Range(0f, 1f);
+            foreach (KeyValuePair<Vector2, T> entityWeight in entityWeights)
+            {
+                if (entityWeight.Key.x <= randomWeight && entityWeight.Key.y >= randomWeight)
+                {
+                    return entityWeight.Value;
+                }
+            }
+        }
+    }
+}
