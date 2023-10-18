d6f0b7bd491f1a083259d7063a968f3cde7775f3
cty288 <chentim.sh@gmail.com>
2023-10-18 12:58:26 -0400

    default modifiers for vectors

--- Assets/02. Scripts/Runtime/DataFramework/Common/ViewControllers/Entities/AbstractCreatureViewController.cs
+++ Assets/02. Scripts/Runtime/DataFramework/Common/ViewControllers/Entities/AbstractCreatureViewController.cs
@@ -1,4 +1,5 @@
-﻿using BehaviorDesigner.Runtime;
+﻿using System;
+using BehaviorDesigner.Runtime;
 using Runtime.DataFramework.Entities;
 using Runtime.DataFramework.Entities.ClassifiedTemplates.CustomProperties;
 using Runtime.DataFramework.Entities.ClassifiedTemplates.Damagable;
@@ -12,6 +13,19 @@ namespace Runtime.DataFramework.ViewControllers.Entities {
 	public interface ICreatureViewController : IDamageableViewController {
 		public BoxCollider SpawnSizeCollider { get; }
 	}
+
+	[Serializable]
+	public struct ItemDropCollection {
+		public int Rarity;
+		public ItemDropInfo[] ItemDropInfos;
+	}
+	
+	[Serializable]
+	public struct ItemDropInfo {
+		public GameObject prefab;
+		public Vector2Int dropCountRange;
+		public float dropChance;
+	}
 	
 	/// <summary>
 	/// An abstract view controller for creature entity (like player, enemy, etc.)
@@ -19,7 +33,10 @@ namespace Runtime.DataFramework.ViewControllers.Entities {
 	/// <typeparam name="T"></typeparam>
 	/// <typeparam name="TEntityModel"></typeparam>
 	public abstract class AbstractCreatureViewController<T> : AbstractDamagableViewController<T>, ICreatureViewController
-		where T : class, IHaveCustomProperties, IHaveTags, IDamageable, ICreature{
+		where T : class, IHaveCustomProperties, IHaveTags, IDamageable, ICreature {
+		[SerializeField] protected ItemDropCollection[] itemDropCollections;
+		
+			
 		NavMeshAgent navMeshAgent;
 		BehaviorTree behaviorTree;
 		protected override void Awake() {
--- Assets/02. Scripts/Runtime/Levels/GlobalLevelFormulas.cs
+++ Assets/02. Scripts/Runtime/Levels/GlobalLevelFormulas.cs
@@ -45,7 +45,37 @@ namespace _02._Scripts.Runtime.Levels {
 					healthInfo.MaxHealth = Mathf.RoundToInt(healthInfo.MaxHealth * y2);
 					healthInfo.CurrentHealth = healthInfo.MaxHealth;
 					return healthInfo;
+				}else if (baseVal is Vector2) {
+					dynamic vector2 = baseValDynamic;
+					vector2.x = Mathf.RoundToInt(vector2.x * y2);
+					vector2.y = Mathf.RoundToInt(vector2.y * y2);
+					return vector2;
+				}else if (baseVal is Vector3) {
+					dynamic vector3 = baseValDynamic;
+					vector3.x = Mathf.RoundToInt(vector3.x * y2);
+					vector3.y = Mathf.RoundToInt(vector3.y * y2);
+					vector3.z = Mathf.RoundToInt(vector3.z * y2);
+					return vector3;
+				}else if (baseVal is Vector4) {
+					dynamic vector4 = baseValDynamic;
+					vector4.x = Mathf.RoundToInt(vector4.x * y2);
+					vector4.y = Mathf.RoundToInt(vector4.y * y2);
+					vector4.z = Mathf.RoundToInt(vector4.z * y2);
+					vector4.w = Mathf.RoundToInt(vector4.w * y2);
+					return vector4;
+				}else if (baseVal is Vector2Int) {
+					dynamic vector2Int = baseValDynamic;
+					vector2Int.x = Mathf.RoundToInt(vector2Int.x * y2);
+					vector2Int.y = Mathf.RoundToInt(vector2Int.y * y2);
+					return vector2Int;
+				}else if (baseVal is Vector3Int) {
+					dynamic vector3Int = baseValDynamic;
+					vector3Int.x = Mathf.RoundToInt(vector3Int.x * y2);
+					vector3Int.y = Mathf.RoundToInt(vector3Int.y * y2);
+					vector3Int.z = Mathf.RoundToInt(vector3Int.z * y2);
+					return vector3Int;
 				}
+
 				throw new System.Exception(
 					"Type not supported for general enemy ability modifier. Only int and float are supported.");
 			};
@@ -66,7 +96,38 @@ namespace _02._Scripts.Runtime.Levels {
 					healthInfo.MaxHealth = Mathf.RoundToInt(healthInfo.MaxHealth * Mathf.Sqrt(y2));
 					healthInfo.CurrentHealth = healthInfo.MaxHealth;
 					return healthInfo;
+				}else if (baseVal is Vector2) {
+					dynamic vector2 = baseVal;
+					vector2.x = Mathf.RoundToInt(vector2.x * Mathf.Sqrt(y2));
+					vector2.y = Mathf.RoundToInt(vector2.y * Mathf.Sqrt(y2));
+					return vector2;
+				}else if (baseVal is Vector3) {
+					dynamic vector3 = baseVal;
+					vector3.x = Mathf.RoundToInt(vector3.x * Mathf.Sqrt(y2));
+					vector3.y = Mathf.RoundToInt(vector3.y * Mathf.Sqrt(y2));
+					vector3.z = Mathf.RoundToInt(vector3.z * Mathf.Sqrt(y2));
+					return vector3;
+				}else if (baseVal is Vector4) {
+					dynamic vector4 = baseVal;
+					vector4.x = Mathf.RoundToInt(vector4.x * Mathf.Sqrt(y2));
+					vector4.y = Mathf.RoundToInt(vector4.y * Mathf.Sqrt(y2));
+					vector4.z = Mathf.RoundToInt(vector4.z * Mathf.Sqrt(y2));
+					vector4.w = Mathf.RoundToInt(vector4.w * Mathf.Sqrt(y2));
+					return vector4;
+				}else if (baseVal is Vector2Int) {
+					dynamic vector2Int = baseVal;
+					vector2Int.x = Mathf.RoundToInt(vector2Int.x * Mathf.Sqrt(y2));
+					vector2Int.y = Mathf.RoundToInt(vector2Int.y * Mathf.Sqrt(y2));
+					return vector2Int;
+				}else if (baseVal is Vector3Int) {
+					dynamic vector3Int = baseVal;
+					vector3Int.x = Mathf.RoundToInt(vector3Int.x * Mathf.Sqrt(y2));
+					vector3Int.y = Mathf.RoundToInt(vector3Int.y * Mathf.Sqrt(y2));
+					vector3Int.z = Mathf.RoundToInt(vector3Int.z * Mathf.Sqrt(y2));
+					return vector3Int;
 				}
+				
+				
 				throw new System.Exception(
 					"Type not supported for general enemy ability modifier. Only int and float are supported.");
 			};
