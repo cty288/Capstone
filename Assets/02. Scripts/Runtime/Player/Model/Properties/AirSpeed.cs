53c11411b3af357cdef878e4a7b3f51c2ebe20c7
cty288 <chentim.sh@gmail.com>
2023-10-27 18:25:36 -0400

    DP-404: player air drag and air speed property

---
+++ Assets/02. Scripts/Runtime/Player/Model/Properties/AirSpeed.cs
@@ -0,0 +1,20 @@
+﻿using Runtime.DataFramework.Properties;
+
+namespace Runtime.Player.Properties {
+	
+	public interface IAirSpeedProperty : IProperty<float>, ILoadFromConfigProperty { }
+
+	public class AirSpeed: AbstractLoadFromConfigProperty<float>, IAirSpeedProperty {
+		protected override IPropertyDependencyModifier<float> GetDefautModifier() {
+			return new EmptyModifier<float>();
+		}
+
+		protected override PropertyName GetPropertyName() {
+			return PropertyName.air_speed;
+		}
+
+		public override PropertyNameInfo[] GetDefaultDependentProperties() {
+			return null;
+		}
+	}
+}
