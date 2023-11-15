43c0c66be3f1c29aa999b7ca7fdd371f9f060ad8
cty288 <chentim.sh@gmail.com>
2023-11-15 15:44:50 -0500

    Revert "Revert "Merge branch 'new-main-joon' into develop""
    This reverts commit bbead5cd994c8260b2eafc308ffbda9d7ca364f1.

---
+++ Assets/02. Scripts/Runtime/GameResources/Model/Base/RawMaterialEntity.cs
@@ -0,0 +1,13 @@
+﻿using Runtime.Utilities.ConfigSheet;
+
+namespace Runtime.GameResources.Model.Base {
+	
+	public interface IRawMaterialEntity : IResourceEntity {
+		
+	}
+	public abstract class RawMaterialEntity<T> : ResourceEntity<T>, IRawMaterialEntity where T : ResourceEntity<T>, new() {
+		protected override ConfigTable GetConfigTable() {
+			return ConfigDatas.Singleton.RawMaterialEntityConfigTable;
+		}
+	}
+}
---
+++ Assets/02. Scripts/Runtime/GameResources/ViewControllers/Instances/AbstractRawMaterialViewController.cs
@@ -0,0 +1,16 @@
+﻿using Runtime.DataFramework.Entities;
+using Runtime.Enemies.Model.Builders;
+using Runtime.GameResources.Model.Base;
+using Runtime.GameResources.Model.Builder;
+
+namespace Runtime.GameResources.ViewControllers.Instances {
+	public abstract class AbstractRawMaterialViewController<T> : AbstractResourceViewController<T> 
+		where  T : class, IRawMaterialEntity, new(){
+		protected override IEntity OnInitEntity() {
+			RawMaterialBuilder<T> builder = entityModel.GetRawMaterialBuilder<T>();
+			return OnInitResourceEntity(builder);
+		}
+
+		protected abstract IEntity OnInitResourceEntity(RawMaterialBuilder<T> builder);
+	}
+}
