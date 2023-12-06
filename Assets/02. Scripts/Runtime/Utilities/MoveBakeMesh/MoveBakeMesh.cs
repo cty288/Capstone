5b536f20fbe4a1fb3bf4317dd1b2938ad6ca079b
cty288 <chentim.sh@gmail.com>
2023-12-06 01:44:28 -0500

    MovingBakedSkinMeshRenderer Objects

---
+++ Assets/02. Scripts/Runtime/Utilities/MoveBakeMesh/MoveBakeMesh.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Framework;
+using MikroFramework;
+using MikroFramework.Architecture;
+using MikroFramework.Pool;
+using MikroFramework.ResKit;
+using UnityEngine;
+
+public class MoveBakeMesh : AbstractMikroController<MainGame> {
+	//[SerializeField]
+	private SkinnedMeshRenderer[] skinList;
+	[SerializeField]
+	public int spaceTime = 5;
+	[SerializeField]
+	private float initialTransparency = 0.8f;
+	[SerializeField]
+	private float fadeSpeed = 1;
+	[SerializeField]
+	private Color color = new Color(1f, 0.2f, 0, 1);
+	[SerializeField]
+	private float globalScale = 1;
+	[SerializeField]
+	private Material overrideMat = null;
+	
+	
+	private int countTime = 0;
+	private Vector3 oldPos;
+
+	private SafeGameObjectPool pool;
+	// Start is called before the first frame update
+	void Awake() {
+		skinList = this.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+		pool = GameObjectPoolManager.Singleton.CreatePoolFromAB("MoveBasedMeshObj", null, 50, 200, out _);
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		countTime++;
+		if(countTime % spaceTime == 0)
+		{
+			BakeFun();
+		}
+	}
+
+	private void BakeFun()
+	{
+		if(Vector3.Distance(this.gameObject.transform.position,oldPos)>0)
+		{
+			oldPos = this.gameObject.transform.position;
+			for (int i = 0; i < skinList.Length; i++)
+			{
+				Mesh mesh = new Mesh();
+				skinList[i].BakeMesh(mesh);
+				GameObject go = pool.Allocate();
+				MoveBakedMeshRenderObject mbm = go.GetComponent<MoveBakedMeshRenderObject>();
+
+				mbm.Initialize(initialTransparency, fadeSpeed, color, globalScale, mesh, overrideMat);
+				
+				go.transform.position = this.transform.position;
+				go.transform.rotation = this.transform.rotation;
+				
+			}
+		}
+
+	}
+}
