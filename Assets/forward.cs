33bc2477c1b28729070ac8045b07b311d15939ac
Joon Lee <wl2357@nyu.edu>
2024-01-16 03:07:22 +0900

    spine animator stuff

---
+++ Assets/forward.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(this.transform.forward * 5 * Time.deltaTime);
+    }
+}
