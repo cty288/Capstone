60269dc2dad87b3d42d26923d2dbff84217f64ad
cty288 <chentim.sh@gmail.com>
2024-02-23 01:13:53 -0500

    Mineral harvesting robot skill complete

---
+++ Assets/02. Scripts/Runtime/Skills/ViewControllers/Instances/MineralRobotSkill/MineralRobotSkillInHandViewController.cs
@@ -0,0 +1,155 @@
+﻿using _02._Scripts.Runtime.Skills.Model.Builders;
+using _02._Scripts.Runtime.Skills.Model.Instance.MineralRobotSkill;
+using _02._Scripts.Runtime.Skills.Model.Instance.TurretSkill;
+using _02._Scripts.Runtime.Skills.ViewControllers.Base;
+using _02._Scripts.Runtime.Utilities;
+using Cysharp.Threading.Tasks;
+using MikroFramework.Architecture;
+using Runtime.GameResources.Model.Base;
+using Runtime.Player.ViewControllers;
+using Runtime.Spawning;
+using Runtime.Utilities;
+using Runtime.Utilities.AnimatorSystem;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace _02._Scripts.Runtime.Skills.ViewControllers.Instances.MineralRobotSkill {
+	public class MineralRobotSkillInHandViewController : AbstractInHandSkillViewController<Model.Instance.MineralRobotSkill.MineralRobotSkill> {
+		private bool usedBefore = false;
+
+		[SerializeField] private GameObject robotPrefab;
+		[SerializeField] private float optimalSpawnDistance = 3f;
+		protected override void OnBindEntityProperty() {
+			
+		}
+
+		protected override void OnEntityStart() {
+			base.OnEntityStart();
+			this.RegisterEvent<OnPlayerAnimationEvent>(OnPlayerAnimationEvent)
+				.UnRegisterWhenGameObjectDestroyedOrRecycled(gameObject);
+		}
+
+		private void OnPlayerAnimationEvent(OnPlayerAnimationEvent e) {
+			if (e.AnimationName == "OnRemoteControllerUse") {
+				DoUseSkill();
+			}
+		}
+
+		private async UniTask DoUseSkill() {
+			MineralRobotEntity entity = await SpawnTurret();
+			UseSkill(()=>{OnUseSuccess(entity);}, () => { OnUseFail(entity); });
+		}
+
+		private void OnUseFail(MineralRobotEntity entity) {
+			entity?.RecycleToCache();
+		}
+
+		private void OnUseSuccess(MineralRobotEntity entity) {
+			BoundEntity.OnSpawnRobot(entity);
+		}
+
+		private async UniTask<MineralRobotEntity> SpawnTurret() {
+			Vector3 spawnPosition = ownerGameObject.transform.position +
+			                        ownerGameObject.transform.forward * optimalSpawnDistance;
+
+			NavMesh.SamplePosition(spawnPosition, out NavMeshHit hit, 5, NavMeshHelper.GetSpawnableAreaMask());
+			spawnPosition = hit.position;
+
+			BoxCollider collider = robotPrefab.GetComponent<MineralRobotEntityViewController>().GetSpawnSizeCollider();
+			float height = collider.size.y;
+			
+			NavMeshFindResult result = await SpawningUtility.FindNavMeshSuitablePosition(gameObject,
+				() => collider,
+				spawnPosition, 180, NavMeshHelper.GetSpawnableAreaMask(), default, 1, 5, 100);
+
+
+			if (!result.IsSuccess) {
+				//spawn behind the player
+				spawnPosition = ownerGameObject.transform.position -
+				                ownerGameObject.transform.forward * optimalSpawnDistance;
+				NavMesh.SamplePosition(spawnPosition, out hit, 5, NavMeshHelper.GetSpawnableAreaMask());
+				spawnPosition = hit.position;
+
+
+				result = await SpawningUtility.FindNavMeshSuitablePosition(gameObject,
+					() => collider,
+					spawnPosition, 180, NavMeshHelper.GetSpawnableAreaMask(), default, 1, 5, 100);
+
+				if (!result.IsSuccess) {
+				
+					int maxAttempt = 500;
+					while (maxAttempt > 0) {
+						//pick random direction + position
+						Vector3 randomDirection = Random.insideUnitSphere;
+						randomDirection.y = 0;
+						randomDirection.Normalize();
+						spawnPosition = ownerGameObject.transform.position +
+						                randomDirection * Random.Range(0.5f, optimalSpawnDistance);
+						
+						NavMesh.SamplePosition(spawnPosition, out hit, 5, NavMeshHelper.GetSpawnableAreaMask());
+						spawnPosition = hit.position;
+						
+						result = await SpawningUtility.FindNavMeshSuitablePosition(gameObject,
+							() => collider,
+							spawnPosition, 180, NavMeshHelper.GetSpawnableAreaMask(), default, 1, 5, 100);
+						if (result.IsSuccess) {
+							break;
+						}
+
+						maxAttempt -= result.UsedAttempts;
+					}
+					
+					if (!result.IsSuccess) {
+						return null;
+					}
+				}
+			}
+
+			var turret = Instantiate(robotPrefab, result.TargetPosition, result.RotationWithSlope);
+			turret.transform.position -= turret.transform.up * height;
+			
+			MineralRobotEntity entity = turret.GetComponent<MineralRobotEntityViewController>().OnBuildEntity(
+				BoundEntity.GetLevel(),
+				BoundEntity.GetCustomPropertyOfCurrentLevel<int>("limit"),
+				BoundEntity.GetCustomPropertyOfCurrentLevel<float>("interval"),
+				BoundEntity.GetCustomPropertyOfCurrentLevel<int>("count"));
+		
+			MineralRobotEntityViewController viewController = turret.GetComponent<MineralRobotEntityViewController>();
+			viewController.InitWithID(entity.UUID);
+			viewController.SetFollowTarget(ownerGameObject.transform);
+			//face the player, but keep x and z rotation 0
+			viewController.transform.LookAt(ownerGameObject.transform);
+			viewController.transform.localEulerAngles = new Vector3(0, viewController.transform.localEulerAngles.y, 0);
+
+			return entity;
+		}
+
+		public override void OnItemStartUse() {
+			
+		}
+
+		public override void OnItemStopUse() {
+			if (usedBefore) {
+				return;
+			}
+			usedBefore = true;
+			this.SendCommand<PlayerAnimationCommand>
+				(PlayerAnimationCommand.Allocate("ItemUse", AnimationEventType.Trigger, 0));
+
+		}
+
+		public override void OnItemUse() {
+			
+		}
+
+		protected override IResourceEntity OnInitSkillEntity(SkillBuilder<Model.Instance.MineralRobotSkill.MineralRobotSkill> builder) {
+			return builder.FromConfig().Build();
+		}
+
+		public override void OnRecycled() {
+			base.OnRecycled();
+			usedBefore = false;
+			this.SendCommand<PlayerAnimationCommand>(PlayerAnimationCommand.Allocate("Shoot", AnimationEventType.ResetTrigger, 0));
+		}
+	}
+}
---
+++ Assets/SC_Post_Effects/SC Post Effects/Runtime/BlackBars/BlackBarsRenderer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+namespace SCPE
+{
+    public class BlackBarsRenderer : ScriptableRendererFeature
+    {
+        class BlackBarsRenderPass : PostEffectRenderer<BlackBars>
+        {
+            public BlackBarsRenderPass(EffectBaseSettings settings)
+            {
+                this.settings = settings;
+                shaderName = ShaderNames.BlackBars;
+                ProfilerTag = GetProfilerTag();
+            }
+
+            public void Setup(ScriptableRenderer renderer)
+            {
+                this.cameraColorTarget = GetCameraTarget(renderer);
+                volumeSettings = VolumeManager.instance.stack.GetComponent<BlackBars>();
+                
+                if(volumeSettings && volumeSettings.IsActive()) renderer.EnqueuePass(this);
+            }
+
+            protected override void ConfigurePass(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+            {
+                if (!volumeSettings) return;
+
+                base.ConfigurePass(cmd, cameraTextureDescriptor);
+            }
+
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                if (ShouldRender(renderingData) == false) return;
+
+                var cmd = GetCommandBuffer(ref renderingData);
+
+                CopyTargets(cmd, renderingData);
+
+                Material.SetVector("_Size", new Vector2(volumeSettings.size.value / 10f, volumeSettings.maxSize.value * 5));
+
+                FinalBlit(this, context, cmd, renderingData, (int)volumeSettings.mode.value);
+            }
+        }
+
+        BlackBarsRenderPass m_ScriptablePass;
+        [SerializeField]
+        public EffectBaseSettings settings = new EffectBaseSettings(false);
+        
+        public override void Create()
+        {
+            m_ScriptablePass = new BlackBarsRenderPass(settings);
+            m_ScriptablePass.renderPassEvent = settings.injectionPoint;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            m_ScriptablePass.Setup(renderer);
+        }
+    }
+}
