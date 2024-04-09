4b1e9be89468115ff76c13c1d48141f74dd9d84d
Ryan Zhang <zhangryan09@gmail.com>
2024-04-09 16:49:40 -0400

    beserker stomp damage

--- Assets/02. Scripts/Runtime/BehaviorDesigner/Tasks/EnemyAction/Berserker/BerserkerStomp.cs
+++ Assets/02. Scripts/Runtime/BehaviorDesigner/Tasks/EnemyAction/Berserker/BerserkerStomp.cs
@@ -3,8 +3,12 @@ using _02._Scripts.Runtime.Utilities;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using Cysharp.Threading.Tasks;
+using MikroFramework;
+using MikroFramework.Pool;
 using  Runtime.BehaviorDesigner.Tasks.EnemyAction;
+using Runtime.DataFramework.Entities.ClassifiedTemplates.Damagable;
 using Runtime.Enemies.ViewControllers.Instances.Berserker;
+using Runtime.Weapons.ViewControllers.Base;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -33,11 +37,14 @@ namespace _02._Scripts.Runtime.BehaviorDesigner.Tasks.EnemyAction
         private Transform _player;
 
         private LayerMask mask = LayerMask.GetMask("Default", "Ground", "Wall");
+        private SafeGameObjectPool pool;
+        public SharedGameObject stompImpact;
         
         // **** Values to add to spreadsheet.
         private float _stompHeight = 50f;
         private float _stompTime = 1f;
         private float _readySpeed = 50f;
+        private int _damage = 40;
         
         public override void OnStart()
         {
@@ -53,6 +60,8 @@ namespace _02._Scripts.Runtime.BehaviorDesigner.Tasks.EnemyAction
             _progress = 0;
 
             taskStatus = TaskStatus.Running;
+            
+            pool = GameObjectPoolManager.Singleton.CreatePool(stompImpact.Value, 2, 5);
         }
         
         public override TaskStatus OnUpdate()
@@ -110,6 +119,12 @@ namespace _02._Scripts.Runtime.BehaviorDesigner.Tasks.EnemyAction
 
             if (_progress >= 1)
             {
+                GameObject explosion = pool.Allocate();
+                explosion.transform.position = this.gameObject.transform.position;
+                explosion.GetComponent<IExplosionViewController>().
+                    Init(enemyEntity.CurrentFaction.Value, 
+                        _damage,4, gameObject,
+                        gameObject.GetComponent<ICanDealDamage>());
                 _progress = 0;
                 _phase = Phase.Complete;
             }
