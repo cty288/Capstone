ff076a1ba9749e9d19b42c30a360058fb8f2d27d
Allyson <abc9295@nyu.edu>
2024-04-09 01:02:40 -0400

    start stagger

--- Assets/02. Scripts/Runtime/Enemies/ViewControllers/Instances/Berserker/Berserker.cs
+++ Assets/02. Scripts/Runtime/Enemies/ViewControllers/Instances/Berserker/Berserker.cs
@@ -10,6 +10,7 @@ using Runtime.DataFramework.Properties.CustomProperties;
 using Runtime.Enemies.Model;
 using Runtime.Enemies.Model.Builders;
 using Runtime.Enemies.ViewControllers.Base;
+using Runtime.Utilities.Collision;
 using UnityEngine;
 
 namespace Runtime.Enemies.ViewControllers.Instances.Berserker {
@@ -19,6 +20,8 @@ namespace Runtime.Enemies.ViewControllers.Instances.Berserker {
 		public override string EntityName { get; set; } = "Berserker";
 
 		public List<BerserkerNode> Nodes;
+		public int StaggerThreshold;
+		public int StaggerDamage = 0;
 		
 		protected override void OnEntityStart(bool isLoadedFromSave) {
             
@@ -57,10 +60,29 @@ namespace Runtime.Enemies.ViewControllers.Instances.Berserker {
 		private bool deathAnimationEnd = false;
 
 		[SerializeField] private List<BerserkerNode> nodes;
+		[SerializeField] private HurtBox[] vulerableHurboxes;
+		private HashSet<IHurtbox> hashedVulerableHurboxes = new HashSet<IHurtbox>();
+
+		private float StaggerDelay = 0f;
+		private float lastHitTime = 0f;
+		private float StaggerTick = 0f;
+		private float staggerTimer = 0f;
 		
+		protected override void Awake()
+		{
+			base.Awake();
+			hashedVulerableHurboxes.Clear();
+			foreach (var hurtbox in vulerableHurboxes) {
+				hashedVulerableHurboxes.Add(hurtbox);
+			}
+		}
+
 		protected override void OnEntityStart()
 		{
 			BoundEntity.Nodes = nodes;
+			BoundEntity.StaggerThreshold = BoundEntity.GetCustomDataValue<int>("stagger", "staggerThreshold");
+			StaggerDelay = BoundEntity.GetCustomDataValue<float>("stagger", "staggerDelay");
+			StaggerTick = BoundEntity.GetCustomDataValue<float>("stagger", "staggerTick");
 		}
 
 		protected override void OnEntityTakeDamage(int damage, int currenthealth, ICanDealDamage damagedealer) {
@@ -69,6 +91,20 @@ namespace Runtime.Enemies.ViewControllers.Instances.Berserker {
 
 		protected override void OnEntityHeal(int heal, int currenthealth, IBelongToFaction healer) {
 			
+		}
+
+		protected override void Update()
+		{
+			base.Update();
+			
+			if(Time.time - lastHitTime >= StaggerDelay) {
+				staggerTimer += Time.deltaTime;
+				if (staggerTimer >= StaggerTick)
+				{
+					BoundEntity.StaggerDamage--;
+					staggerTimer = 0f;
+				}
+			}
 		}
 
 		protected override MikroAction WaitingForDeathCondition() {
@@ -85,6 +121,19 @@ namespace Runtime.Enemies.ViewControllers.Instances.Berserker {
 
 		protected override IEnemyEntity OnInitEnemyEntity(EnemyBuilder<BerserkerEntity> builder) {
 			return builder.FromConfig().Build();
+		}
+
+		public override void HurtResponse(HitData data)
+		{
+			if(hashedVulerableHurboxes.Contains(data.Hurtbox)) {
+				BoundEntity.StaggerDamage += data.Damage;
+				lastHitTime = Time.time;
+				if(BoundEntity.StaggerDamage >= BoundEntity.StaggerThreshold) {
+					BoundEntity.StaggerDamage = 0;
+					// Stagger();
+				}
+			}
+			BoundEntity.TakeDamage(data.Damage, data.Attacker,out _, data);
 		}
 
 		public override void OnRecycled() {
