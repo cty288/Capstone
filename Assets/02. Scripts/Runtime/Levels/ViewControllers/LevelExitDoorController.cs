7c52d89e0f5794ca43cd15d80879324856a5b9d5
Allyson <abc9295@nyu.edu>
2024-05-01 02:17:52 -0400

    update door animations

--- Assets/02. Scripts/Runtime/Levels/ViewControllers/LevelExitDoorController.cs
+++ Assets/02. Scripts/Runtime/Levels/ViewControllers/LevelExitDoorController.cs
@@ -5,6 +5,7 @@ using _02._Scripts.Runtime.Levels.Models;
 using _02._Scripts.Runtime.Levels.Systems;
 using Framework;
 using MikroFramework.Architecture;
+using MikroFramework.AudioKit;
 using MikroFramework.Event;
 using Polyglot;
 using Runtime.UI.NameTags;
@@ -12,19 +13,23 @@ using Runtime.Weapons.ViewControllers.CrossHairs;
 using UnityEngine;
 
 public class LevelExitDoorController : AbstractMikroController<MainGame>, ICrossHairDetectable {
-    private GameObject exitDoorGameObject;
+    [SerializeField] private GameObject exitDoorGameObject;
     private ILevelSystem levelSystem;
     private ILevelModel levelModel;
-    private Transform hudSpawnPoint;
+    [SerializeField] private Transform hudSpawnPoint;
     private INameTag spawnedNameTag;
     private GameObject spawnedNameTagGameObject;
     [SerializeField] private bool alwaysOpen = false;
 
-    public BoxCollider SpawnSizeCollider => transform.Find("SpawnSizeCollider").GetComponent<BoxCollider>();
+    [SerializeField] private BoxCollider SpawnSizeCollider;
     
+    [SerializeField] private Animator animator;
+    private static readonly int lower = Animator.StringToHash("Lower");
+    private static readonly int raise = Animator.StringToHash("Raise");
+
     private void Awake() {
-        exitDoorGameObject = transform.Find("ExitDoor").gameObject;
-        hudSpawnPoint = transform.Find("HUDSpawnPoint");
+        // exitDoorGameObject = transform.Find("ExitDoor").gameObject;
+        // hudSpawnPoint = transform.Find("HUDSpawnPoint");
         exitDoorGameObject.SetActive(false);
         levelSystem = this.GetSystem<ILevelSystem>();
         levelModel = this.GetModel<ILevelModel>();
@@ -43,8 +48,30 @@ public class LevelExitDoorController : AbstractMikroController<MainGame>, ICross
 
     }
 
-    
+    private void Start() {
+        LowerDoor();
+    }
 
+    private void LowerDoor()
+    {
+        animator.SetTrigger(lower);
+    }
+    
+    private void RaiseDoor()
+    {
+        animator.SetTrigger(raise);
+    }
+    
+    public void PlayRiseAudio()
+    {
+        AudioSystem.Singleton.Play3DSound("door_rise", transform.position);
+    }
+
+    public void PlayLowerAudio()
+    {
+        AudioSystem.Singleton.Play3DSound("door_lower", transform.position);
+    }
+    
     private void SetExitDoorName(bool isOpen, string localizationName) {
         spawnedNameTag.SetName(Localization.Get(localizationName));
         
@@ -64,6 +91,7 @@ public class LevelExitDoorController : AbstractMikroController<MainGame>, ICross
         }
         
         if(newVal) { 
+            RaiseDoor();
             exitDoorGameObject.SetActive(true);
             SetExitDoorName(true, "EXIT_DOOR_STATE_1");
         }
